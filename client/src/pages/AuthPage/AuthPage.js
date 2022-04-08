75883dd2f010c35c9dcfe5a4b05c82dacee92226
Andrey Ilin <frayz2507@gmail.com>
2022-04-08 18:07:40 +0300

    create auth

--- client/src/pages/AuthPage/AuthPage.js
+++ client/src/pages/AuthPage/AuthPage.js
@@ -1,10 +1,31 @@
 import React from 'react'
+import './AuthPage.css'
 
 function AuthPage() {
   return (
-    <div>
-        
-    </div>
+    <React.Fragment>
+        <div className='cont'>
+            <div className='auth'>
+                <h3 className='auth__name'>Вход</h3>
+                <form className='form__login'>
+                    <div className='row'>
+                        <div className='auth__input__field'>
+                            <input type='text' name='username' className='auth_validate' placeholder='Username'/>
+                        </div>
+                        <div className='auth__input__field'>
+                            <input type='password' name='password' className='auth_validate' placeholder='Password'/>
+                        </div>
+                        <div className='auth__reg'>
+                            <span>Нет аккаунта? <a href='/'>Зарегистрироваться</a> </span>
+                        </div>
+                    </div>
+                    <div className='row'>
+                        <button className='auth__button'>Войти</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </React.Fragment>
   )
 }
 
